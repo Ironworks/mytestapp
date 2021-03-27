d502afa9b0957e7dd839fdff1fe3fde0e2dfb771
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2021-03-27 15:48:54 +0000

    Add test

--- MyTestAppTests/MyTestAppTests.swift
+++ MyTestAppTests/MyTestAppTests.swift
@@ -22,6 +22,10 @@ class MyTestAppTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func test_2() {
+        XCTAssert(true)
+    }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
